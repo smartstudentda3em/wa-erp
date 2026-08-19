770b93ab49770ba69669af4ef2e5518827d16f14
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2026-08-19 19:51:48 +0300

    Revert error dumper

--- public/index.php
+++ public/index.php
@@ -12,16 +12,6 @@ if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-try {
-    // Bootstrap Laravel and handle the request...
-    (require_once __DIR__.'/../bootstrap/app.php')
-        ->handleRequest(Request::capture());
-} catch (\Throwable $e) {
-    echo "<h2 style='color:red'>Fatal Error Caught:</h2>";
-    echo "<pre style='background:#111; color:#fff; padding:20px;'>";
-    echo "Message: " . $e->getMessage() . "\n";
-    echo "File: " . $e->getFile() . ":" . $e->getLine() . "\n\n";
-    echo $e->getTraceAsString();
-    echo "</pre>";
-    exit;
-}
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
